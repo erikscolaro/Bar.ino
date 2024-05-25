d503af9a35f9cb8b96a14edfeb8023433a10505a
Erik Scolaro <erikscolaro31@gmail.com>
2024-05-26 00:46:43 +0200

    refined actual project methods

--- include/recipe.settings.h
+++ include/recipe.settings.h
@@ -1,4 +1,8 @@
 
 #define INGREDIENTS_NUM 10
 #define STEPS_NUM 10
-#define RECIPE_NAME_LEN 32
+#define RECIPE_NAME_LEN 24
+
+#define ERROR_MSG_not_implemented "Questa pagina non e' ancora stata implementata."
+#define ERROR_MSG_select_dimension "Seleziona la dimensione di un cocktail prima di procedere."
+#define ERROR_MSG_not_enought_ingredients "Non ci sono sufficienti ingredienti. Prova a farlo piu' piccolo o bevi acqua!"
